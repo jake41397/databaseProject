6e2e21cab0ab93f6ef7d1d95c3afcacfe32210f5
Jacob Knudson <jake.knudson@knights.ucf.edu>
2019-04-11 10:47:04 -0400

    fresh

--- config.php
+++ config.php
@@ -4,7 +4,6 @@
    define('DB_PASSWORD', 'bison41397');
    define('DB_DATABASE', 'surveytoday');
    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-   $seed = 789;
 
    session_start(); // starts the session
    $_SESSION['url'] = $_SERVER['REQUEST_URI']; // i.e. "about.php"
